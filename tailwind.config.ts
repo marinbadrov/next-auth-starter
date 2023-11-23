196c7c79a8ab727cefab2a25851b0755b9b1656a
marinbadrov <marin.badrov.dev@gmail.com>
2023-11-23 16:26:58 +0100

    unused code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,12 +12,6 @@ const config: Config = {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
-
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [
